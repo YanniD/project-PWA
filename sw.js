660c5a9cdbf0af405afd2f40f789a0081486f41a
YanniD <yanni.derous@student.howest.be>
2018-05-16 20:48:36 +0200

    sw13
    qsf

--- sw.js
+++ sw.js
@@ -20,9 +20,8 @@ self.addEventListener('fetch', function(event) {
         caches.match(event.request).then(function(resp) {
             return resp || fetch(event.request).then(function(response) {
                 return caches.open('tdee-static-v2').then(function(cache) {
-
                     cache.put(event.request, response.clone());
-                    console.log("service worker fetch");
+                   console.log("fetch : " + event.request);
                     return response;
                 }).catch(err => console.log(err));
             }).catch(err => console.log(err));
